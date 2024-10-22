38c9179732b6a7da0679ad1d8bf15f80af8a5c12
marco rojouan <marcoprojouan@outlook.fr>
2024-10-22 16:07:39 +0200

    feat step3 done

--- biomePokedex/src/components/PokemonCard.tsx
+++ biomePokedex/src/components/PokemonCard.tsx
@@ -1,8 +1,21 @@
+
+const pokemonList = [
+    {
+        name: "Onion Turtle",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    },
+    {
+        name: "Mew",
+    },
+]
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
+
 
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
     </figure>;
 }
 
