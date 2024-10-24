115308557393e0a9e5272db861190efe5d4a4914
marco rojouan <marcoprojouan@outlook.fr>
2024-10-24 16:39:34 +0200

    commiting

--- workshopReact1/vite-project/src/App.tsx
+++ workshopReact1/vite-project/src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import HelloWorld from './helloWorld'
+
 import Avatar from './components/Avatar'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
     <>
@@ -15,11 +15,13 @@ function App() {
         image={"https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png"}
         firstName={"Bart"}
         lastName={"Simpson"}
+
       />
       <Avatar
         image={"https://www.stickees.com/files/cartoon/the-simpsons/2248-homer-simpson-happy.png"}
         firstName={"Homer"}
         lastName={"Simpson"}
+
       />
     </>
   )
--- workshopReact1/vite-project/src/components/Avatar.tsx
+++ workshopReact1/vite-project/src/components/Avatar.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 type AvatarType = {
     image: string;
     firstName: string;
     lastName: string;
+
 }
 
 function Avatar({ image, firstName, lastName }: AvatarType) {
-    return <section>
+
+    const [donutsCount, setDonutsCount] = useState(0);
+
+    return <>
         <img src={image} alt={`${firstName} ${lastName}`} />
-        <p>{`${firstName} ${lastName}`}</p>
-    </section>
+        <p>
+            {`${firstName} ${lastName.toUpperCase()}`}
+            <button type="button" onClick={() => setDonutsCount(donutsCount + 1)} > 🍩
+            </button>
+            {donutsCount}
+        </p>
+
+    </>
 
 }
 
